827cbb386ca437a504edb36110e2a4ae8b2c2a9c
root <1021600405@qq.com>
2020-04-09 16:56:50 +0800

    003

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cp /var/lib/jenkins/workspace/jenkins_demo/target/jenkins.jar /jarRun/main.jar
+sudo cp /var/lib/jenkins/workspace/jenkins_demo/target/jenkins.jar /jarRun/main.jar
 java -jar /jarRun/main.jar
