8d0397bb52ec7257f5d15260ca9159ed464f4502
root <1021600405@qq.com>
2020-04-10 10:13:26 +0800

    004

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-sudo cp /var/lib/jenkins/workspace/jenkins_demo/target/jenkins.jar /jarRun/main.jar
-java -jar /jarRun/main.jar
+docker stop ${project.artifactId}
+docker rm ${project.artifactId}
+docker run -itd --name ${project.artifactId} -p 31002:31002 118.190.2.114:5000/${project.artifactId}:${project.version}
