312f52967d32c8cd6d512a888a794786ab510239
root <1021600405@qq.com>
2020-04-10 11:02:04 +0800

    004

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker stop ${project.artifactId}
-docker rm ${project.artifactId}
-docker run -itd --name ${project.artifactId} -p 31002:31002 118.190.2.114:5000/${project.artifactId}:${project.version}
+docker stop jenkins
+docker rm jenkins
+docker run -itd --name jenkins -p 31002:31002 118.190.2.114:5000/jenkins:1.0.0.RELEASE
