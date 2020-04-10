7764a1b0dfd4c75326b11a9905ce46b2b2fe7a18
root <1021600405@qq.com>
2020-04-10 17:59:12 +0800

    004

--- src/main/java/com/example/jenkins/controller/MainController.java
+++ src/main/java/com/example/jenkins/controller/MainController.java
@@ -22,6 +22,6 @@ public class MainController {
 
     @RequestMapping("main")
     public String main(){
-        return  applictionName+" hello";
+        return  applictionName+" hello001";
     }
 }
